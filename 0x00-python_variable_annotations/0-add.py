b80b131e89ce30e43463b67e1f13eba2efad46b3
prototypedave <popupgfj@gmail.com>
2022-08-06 01:29:42 +0000

    pep8 style error

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
-# Basic annotations - add 
+""" Basic annotations - add """
 
 
 def add(a: float, b: float) -> float:
-    #returns sum as a float
-    
-    return a + b
+    """ type-annotated function add that takes a float a and a float b
+        as arguments and returns their sum as a float.
+    """
+    return a + 
