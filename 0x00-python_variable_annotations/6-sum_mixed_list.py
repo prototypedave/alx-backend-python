d166c5fee690ae9a18d5ce54605a1c64526058de
prototypedave <popupgfj@gmail.com>
2022-08-06 01:49:38 +0000

    Pycodestyle validation

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -2,6 +2,7 @@
 """ Complex types - mixed list """
 from typing import List, Union
 
+
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """taes in a mix of floats and integers and returns the sum as a float """
     return sum(mxd_lst)
