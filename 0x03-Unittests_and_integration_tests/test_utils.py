7782e1173bd7a02bc6cafec3b48e08d0a6b9a9dc
prototypedave <popupgfj@gmail.com>
2022-09-03 00:07:33 +0000

    corrections

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,7 +3,6 @@
 Parameterize a unit test
 """
 
-
 import unittest
 from unittest.mock import patch
 from parameterized import parameterized
@@ -21,34 +20,34 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map, path, expected):
         """ test accss of the path from a nested map """
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
     
     """ values that can raise error but are exceptional """
     @parameterized.expand([
         ({}, ("a",)),
         ({"a": 1}, ("a", "b"))
     ])
-
     def test_access_nested_map_exception(self, nested_map, path):
     """ raises exception error from the code """
         with self.assertRaises(KeyError) as error:
             access_nested_map(nested_map, path)
         self.assertEqual(error.exception.args[0], path[-1])
 
+
 class TestGetJson(unittest.TestCase):
     """ test class that inherits from unittest """
-    
     """ assigns data to prevent delay from loading over a network """
     @parameterized.expand([
         ("http://example.com", {"payload": True}),
         ("http://holberton.io", {"payload": False})
     ])
     @patch('test_utils.get_json')
- 
     def test_get_json(self, test_url, test_payLoad, mock_get):
         """ test if the function works without loading """
         mock_get.return_value = test_payLoad
         result = get_json(test_url)
         self.assertEqual(result, test_payLoad)
+
 
 class TestMemoize(unittest.TestCase):
     """ class that extends from unittest """
